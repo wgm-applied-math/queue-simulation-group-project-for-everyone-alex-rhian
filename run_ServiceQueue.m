fc18383310fada1f79ad116df7990b39f03434cf
AlexBaham1999 <bahama@g.cofc.edu>
2024-02-19 19:53:40 -0500

    Histograms
    Histograms added - gonna add theoretical values later

--- run_ServiceQueue.m
+++ run_ServiceQueue.m
@@ -73,20 +73,43 @@ timeserved = sum(TotalServed)/qlength;
 SimulationSteadyStates = [totaltimeinsystem, totaltimewaiting, TotalServed];
 TheoreticalSteadyStates = [6.24, 2.31, 3.94];
 
+figw = figure();
+tw = tiledlayout(figw, 1, 1);
+axw = nexttile(tw);
+hold(axw, "on");
 
+Wh = histogram(axw, W, Normalization = "probability", BinMethod = "auto");
+
+figwq = figure();
+twq = tiledlayout(figwq, 1, 1);
+axwq = nexttile(twq);
+hold(axwq, "on");
+
+Wqh = histogram(axwq, WQ, Normalization = "probability", BinMethod = "auto");
+
+figts = figure();
+tts = tiledlayout(figts, 1, 1);
+axts = nexttile(tts);
+hold(axts, "on");
+
+TSh = histogram(axts, TotalServed, Normalization = "probability", BinMethod = "auto");
 
 % Start with a histogram.  The result is an empirical PDF, that is, the
 % area of the bar at horizontal index n is proportional to the fraction of
 % samples for which there were n customers in the system.
-h = histogram(NInSystem, Normalization = "probability", BinMethod = "integers");
+
+fig2 = figure();
+t2 = tiledlayout(1,1);
+ax2 = nexttile(t2);
+h = histogram(ax2, NInSystem, Normalization = "probability", BinMethod = "integers");
 
 % MATLAB-ism: Once you've created a picture, you can use "hold on" to cause
 % further plotting function to work with the same picture rather than
 % create a new one.
-hold on;
+hold(ax2, 'on');
 theoryprob = [9/26, 9/26, 3/13, 1/13];
 xvaluesshifted = [0, 1, 2, 3];
-plot(xvaluesshifted, theoryprob, 'o');
+plot(ax2, xvaluesshifted, theoryprob, 'o');
 
 % For comparison, plot the theoretical results for a M/M/1 queue.
 % The agreement isn't all that good unless you run for a long time, say
@@ -100,7 +123,7 @@ P(1) = P0;
 for n = 1:nMax
     P(1+n) = P0 * rho^n;
 end
-plot(ns, P, 'o', MarkerEdgeColor='k', MarkerFaceColor='r');
+plot(ax2, ns, P, 'o', MarkerEdgeColor='k', MarkerFaceColor='r');
 
 % This sets some paper-related properties of the figure so that you can
 % save it as a PDF and it doesn't fill a whole page.
