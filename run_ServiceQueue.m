b2f9dc89a1d09df434f562ecc1c01b56ae0d36b5
AlexBaham1999 <bahama@g.cofc.edu>
2024-02-21 16:31:06 -0500

    2 servers
    separate branch for two server scenario

--- run_ServiceQueue.m
+++ run_ServiceQueue.m
@@ -7,7 +7,7 @@
 n_samples = 100;
 
 % Each sample is run up to a maximum time of 1000.
-max_time = 1000;
+max_time = 480;
 
 % Record how many customers are in the system at the end of each sample.
 NInSystemSamples = cell([1, n_samples]);
@@ -47,21 +47,85 @@ NInSystem = vertcat(NInSystemSamples{:});
 % f(*args).
 
 %% Make a picture
+qlength = length(q.Served);
 
-% Make a figure with one set of axes.
-fig = figure();
-t = tiledlayout(fig,1,1);
-ax = nexttile(t);
+W = zeros(1, qlength);
 
-% MATLAB-ism: Once you've created a picture, you can use "hold on" to cause
-% further plotting function to work with the same picture rather than
-% create a new one.
-hold(ax, 'on');
+for n = 1:qlength
+    W(1, n) = q.Served{1, n}.DepartureTime - q.Served{1, n}.ArrivalTime;
+end
+
+totaltimeinsystem = sum(W)/qlength;
+
+
+WQ = zeros(1, qlength);
+
+for n = 1:qlength
+    WQ(1, n) = q.Served{1, n}.BeginServiceTime - q.Served{1, n}.ArrivalTime;
+end
+
+totaltimewaiting = sum(WQ)/qlength;
+
+
+TotalServed = zeros(1, qlength);
+
+for n = 1:qlength
+    TotalServed(1, n) = q.Served{1, n}.DepartureTime - q.Served{1, n}.BeginServiceTime;
+end
+
+timeserved = sum(TotalServed)/qlength;
+
+
+SimulationSteadyStates = [totaltimeinsystem, totaltimewaiting, timeserved];
+TheoreticalSteadyStates = [5.361, .3024, 5.0586];
+
+figw = figure();
+tw = tiledlayout(figw, 1, 1);
+axw = nexttile(tw);
+hold(axw, "on");
+
+Wh = histogram(axw, W, Normalization = "probability", BinMethod = "auto");
+plot(axw, xline(5.361));
+
+figwq = figure();
+twq = tiledlayout(figwq, 1, 1);
+axwq = nexttile(twq);
+hold(axwq, "on");
+
+Wqh = histogram(axwq, WQ, Normalization = "probability", BinMethod = "auto");
+plot(axwq, xline(.3024));
+
+figts = figure();
+tts = tiledlayout(figts, 1, 1);
+axts = nexttile(tts);
+hold(axts, "on");
+
+TSh = histogram(axts, TotalServed, Normalization = "probability", BinMethod = "auto");
+plot(axts, xline(5.0586));
+
 
 % Start with a histogram.  The result is an empirical PDF, that is, the
 % area of the bar at horizontal index n is proportional to the fraction of
 % samples for which there were n customers in the system.
-h = histogram(ax, NInSystem, Normalization="probability", BinMethod="integers");
+fig2 = figure();
+t2 = tiledlayout(1,1);
+ax2 = nexttile(t2);
+h = histogram(NInSystem, Normalization = "probability", BinMethod = "integers");
+
+
+% MATLAB-ism: Once you've created a picture, you can use "hold on" to cause
+% further plotting function to work with the same picture rather than
+% create a new one.
+hold(ax2, "on");
+theoryprob = [.40315, .40315, .15118, .03779, .004716];
+xvaluesshifted = [0, 1, 2, 3, 4];
+plot(ax2, xvaluesshifted, theoryprob, 'o');
+
+
+%Wt = histogram(totaltimeinsystem, Normalization= "percentage", BinMethod="integers");
+%WQt = histogram(totaltimewaiting, Normalization= "percentage", BinMethod="integers");
+%WmWQt = histogram(timeserved, Normalization= "percentage", BinMethod="integers");
+
 
 % For comparison, plot the theoretical results for a M/M/1 queue.
 % The agreement isn't all that good unless you run for a long time, say
@@ -75,7 +139,14 @@ P(1) = P0;
 for n = 1:nMax
     P(1+n) = P0 * rho^n;
 end
-plot(ax, ns, P, 'o', MarkerEdgeColor='k', MarkerFaceColor='r');
+plot(ax2, ns, P, 'o', MarkerEdgeColor='k', MarkerFaceColor='r');
 
-% Easiest way I've found to save a figure as a PDF file
-exportgraphics(fig, "Service queue histogram.pdf");
+% This sets some paper-related properties of the figure so that you can
+% save it as a PDF and it doesn't fill a whole page.
+% gcf is "get current figure handle"
+% See https://stackoverflow.com/a/18868933/2407278
+fig = gcf;
+fig.Units = 'inches';
+screenposition = fig.Position;
+fig.PaperPosition = [0 0 screenposition(3:4)];
+fig.PaperSize = [screenposition(3:4)];
